cdf440caaea046d55f8c964f39638a1f7c08bf16
muhdsuhayl <2020865648@student.uitm.edu.my>
2022-06-06 09:21:41 +0800

    third commit

---
+++ conn.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+define ('db_user', 'root') :
+define ('db_password', '') ;
+define ("db_host''localhost'):
+define ('db_name','registration')
+
+§conn = mysqli connect (db host, db user, db password, db name) :
+
+if (mysqli connect errno())
+ (
+
+echo "Failed to connect to MySQL:
+)
+?>
+</body>
+</html>
